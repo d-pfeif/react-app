6ec44341652c79470389aabd1a0c6440b69efbb5
Dakota Pfeifer <d-pfeif@outlook.com>
2023-08-14 16:22:26 -0700

    basic css styling for expense items

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <h2>Expense Item in here</h2>
+    <div className="expense-item">
+      <div>March 28th, 2022</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$298.22</div>
+      </div>
+    </div>
   )
 }
 
